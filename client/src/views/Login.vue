17ba0e194dc23d0a4f68dfb59a86c4460a3eafcc
Maurício Leite <mauricioleite1@gmail.com>
2022-01-19 10:16:38 -0300

    Alterações no Sass e Montagem da page Login

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,29 +1,35 @@
 <template>
-  <main id="login-page">
-    <form
-      :class="[styles.container]"
-    >
+  <main id="login-page" :class="[styles.container]">
+    <div :class=[styles.banner]>
+      Left
+    </div>
+    <div :class=[styles.inputs__container]>
       Login
-      <input
-        id="email"
-        placeholder="você@exemplo.com.br"
-        type="email"
-        v-model="email"
-      />
-      <input
-        id="password"
-        type="password"
-        v-model="password"
-      />
-      <button
-        id="btn-login"
-        type="button"
-        @click="login"
-      >
-        Entrar
-      </button>
+      <form>
+        <label for="email"><h3>Email</h3></label>
+        <input
+          id="email"
+          placeholder="você@exemplo.com.br"
+          type="email"
+          v-model="email"
+        />
+        <label for="password"><h3>Senha</h3></label>
+        <input
+          id="password"
+          placeholder="Por favor, informe sua senha"
+          type="password"
+          v-model="password"
+        />
+        <button
+          id="btn-login"
+          type="button"
+          @click="login"
+        >
+          Entrar
+        </button>
+      </form>
 
-    </form>
+    </div>
 
   </main>
 </template>
