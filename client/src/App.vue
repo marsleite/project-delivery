9fa0ea8db9d71c3babe173ee862fd02a859b7ab5
Maurício Leite <mauricioleite1@gmail.com>
2022-01-27 07:21:45 -0300

    Criação e configuração inicial do Header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <router-view/>
+  <Header />
+  <router-view />
 </template>
 
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
 <style lang="scss">
-@import '@/styles/App.scss';
+@import "@/styles/App.scss";
 </style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header :class=[styles.mainHeader]>The header</header>
+</template>
+
+<script>
+import styles from '@/styles/components/Header.module.scss';
+
+export default {
+  name: 'Header',
+  data() {
+    return {
+      styles,
+    };
+  },
+};
+</script>
